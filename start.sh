967c8b0393b626cefdb5c071fc5b2b3bc75470c8
Ochi Shuji <ochimms@ybb.ne.jp>
2023-11-23 18:08:32 +0900

    Modify shell

--- start.sh
+++ start.sh
@@ -1,14 +1,7 @@
 cd /myApp/web
 echo `pwd`
 
-LPATH="../log/"
-ls $LPATH >> /dev/null 2>&1
-
-if [ $? -ne 0]; then
-mkdir $LPATH
-fi
-
-node ./app.js >> "../log/myApplog_`date`.log" 2>&1 &&
+node ./app.js &
 
 if [ $? -ne 0 ]; then
 echo "error"
